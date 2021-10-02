82d351f9129fd9a12c1bf7762613d91b0fbdb8bb
Mayen Akpan <neroxma@yahoo.com>
2021-10-01 21:53:13 -0700

    updated

--- src/context/RecipesContext.js
+++ src/context/RecipesContext.js
@@ -18,6 +18,32 @@ export const RecipeState = ({ children }) => {
   const FIRST_REQ = `https://cors-anywhere.herokuapp.com/https://api.edaman.com/search?q=pizza&&app_id=${APP_ID}&app_key=${APP_KEY}&from=0$to=9`;
   const URL = `https://cors-anywhere.herokuapp.com/https://api.edaman.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${firstRecipe}&to=${lastRecipe}`;
 
+  const getFirstRsult = async () => {
+    setLoading(true);
+    const response = await fetch(FIRST_REQ);
+    const data = await response.json();
+    setRecipes(data);
+    setLoading(false);
+  };
+
+  const searchRecipes = async () => {
+    setError(false);
+    setLoading(true);
+    const response = await fetch(URL);
+    const data = await response.json();
+    if (search.trim() == "" && !data.more) {
+      setLoading(false);
+      setError(true);
+      return;
+    }
+    setRecipes(data.hits);
+    setLoading(false);
+  };
+
+  //   useEffect(()=>{
+  //     getFirstRsult()
+  //   },[])
+
   return (
     <RecipesContext.Provider value={{}}>{children}</RecipesContext.Provider>
   );
